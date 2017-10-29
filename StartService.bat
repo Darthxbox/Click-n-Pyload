0df7a61bb36670151e6ef83ffcb69cfec4422c2c
Sector <stephan.lorbek@aon.at>
2017-10-29 18:28:34 +0100

    Initial Commit

---
+++ StartService.bat
@@ -0,0 +1,8 @@
+
+netsh interface portproxy add v4tov4 listenport=9666 listenaddress=127.0.0.1 connectport=9000 connectaddress=10.0.0.
+netsh interface portproxy add v4tov4 listenport=9666 listenaddress=127.0.0.1 connectport=9000 connectaddress=10.0.0.
+netsh interface portproxy add v4tov4 listenport=9666 listenaddress=127.0.0.1 connectport=9000 connectaddress=10.0.0.
+netsh interface portproxy add v4tov4 listenport=9666 listenaddress=127.0.0.1 connectport=9000 connectaddress=10.0.0.
+netsh interface portproxy add v4tov4 listenport=9666 listenaddress=127.0.0.1 connectport=9000 connectaddress=10.0.0.
+netsh interface portproxy add v4tov4 listenport=9666 listenaddress=127.0.0.1 connectport=9000 connectaddress=10.0.0.
+netsh interface portproxy add v4tov4 listenport=9666 listenaddress=127.0.0.1 connectport=9000 connectaddress=10.0.0.netsh interface portproxy add v4tov4 listenport=9666 listenaddress=127.0.0.1 connectport=9000 connectaddress=10.0.0.netsh interface portproxy add v4tov4 listenport=9666 listenaddress=127.0.0.1 connectport=9000 connectaddress=10.0.0.netsh interface portproxy add v4tov4 listenport=9666 listenaddress=127.0.0.1 connectport=9000 connectaddress=10.0.0.netsh interface portproxy add v4tov4 listenport=9666 listenaddress=127.0.0.1 connectport=9000 connectaddress=10.0.0.
